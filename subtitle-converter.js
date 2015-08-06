f6ba8258c881b10cad7cffdb7885335c2000a93b
Nick Wronski <nick@codeschool.com>
2015-08-06 13:56:13 -0400

    Maintain relative paths in output directory
    When the --o flag is provided to subtitle-converter, we
    should preserve the relative path structure so that files
    with duplicate names are not overwritten in the output
    folder.

--- subtitle-converter.js
+++ subtitle-converter.js
@@ -4,37 +4,68 @@ var path        = require('path');
 var argv        = require('minimist')(process.argv.slice(2));
 var glob        = require('glob');
 var chalk       = require('chalk');
+var mkdirp      = require('mkdirp');
+var errCount = 0,
+    msg = function (message, type) {
+      if (!type) {
+        type = 'error';
+      }
+      var isError = type === 'error',
+          headerColor = isError || type === 'failure' ?
+              'bgRed' : 'bgGreen',
+          header = chalk[headerColor].bold(type.toUpperCase());
+      console.log(header + chalk.white.bold('\t' + message));
+      if (isError) {
+        errCount += 1;
+      }
+    };
 
 glob(argv['i'] || "**/*.srt", {}, function (err, files) {
-  var msg = function (message, type) {
-    if (!type) {
-      type = 'error';
-    }
-    var headerColor = type === 'error' ? 'bgRed' : 'bgGreen',
-        header = chalk[headerColor].bold(type.toUpperCase());
-    console.log(header + chalk.white.bold('\t' + message));
-  }, usePath;
-
   if (err) {
     msg(err.message);
   } else if (!files.length) {
     msg('The path provided did not match any files.');
   } else {
     files.forEach(function (filePath) {
-      var filePathNorm = path.normalize(filePath);
-      var fileName = path.basename(filePathNorm) + '.vtt';
-      var filePathDir = argv['o'] ? argv['o'] : path.dirname(filePathNorm);
-      var usePath = path.resolve(filePathDir) + path.sep;
-      var nextPath = usePath + fileName;
-      var writer = fs.createWriteStream(nextPath);
+      var filePathNorm = path.normalize(filePath),
+          fileName = path.basename(filePathNorm) + '.vtt',
+          filePathDir = path.dirname(filePathNorm),
+          usePath;
+      if (argv['o']) {
+        // Keep relative paths when --o given
+        usePath = path.resolve(argv['o'], path.relative(process.cwd(), filePathDir));
+      } else {
+        usePath = path.resolve(filePathDir);
+      }
+      var nextFile = usePath + path.sep + fileName;
+      mkdirp(usePath, function (err2) {
+        var writer;
+        if (err2) {
+          msg(err2.message)
+        } else {
+          writer = fs.createWriteStream(nextFile);
 
-      fs.createReadStream(filePathNorm)
-        .pipe(srt2vtt())
-        .pipe(writer);
+          fs.createReadStream(filePathNorm)
+            .pipe(srt2vtt())
+            .pipe(writer);
 
-      writer.on('finish', function() {
-        msg(nextPath, 'write');
+          writer.on('finish', function() {
+            msg(nextFile, 'write');
+          });
+
+          writer.on('error', function(e) {
+            msg(e.message);
+          });
+        }
       });
     });
+  }
+});
+
+process.on('exit', function () {
+  if (errCount > 0) {
+    msg('Encountered ' + errCount + ' error' + (errCount !== 1 ? 's' : '') + '.', 'failure');
+  } else {
+    msg('Finished without error.', 'success');
   }
 });
